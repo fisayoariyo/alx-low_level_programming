4020cf4d04b20dd341429a4a03c6d2aea3b28855
Adefisayomi <ariyoadefisayomi@gmail.com>
2023-02-21 03:37:29 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,7 @@
-int putchar(int char)
+#ifndef ALX_H_INCLUDED
+#define ALX_H_INCLUDED
+
+int _putchar(int char)
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -11,3 +14,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif
