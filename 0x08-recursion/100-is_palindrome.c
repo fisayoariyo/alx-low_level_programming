ff9fcb7c96b525aa99bfffacabd54be78540b87e
Adefisayomi <adefisayomiariyo@gmail.com>
2023-03-07 10:21:55 -0800

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,7 @@
+#include "main.h"
 
 int check_pal(char *s, int i, int len);
-int _strlen_recursion(char *s);
+int is_palindrome(char *s);
 
 /**
  * is_palindrome - checks if a string is a palindrome
@@ -12,20 +13,14 @@ int is_palindrome(char *s)
 {
 	if (*s == 0)
 		return (1);
-	return (check_pal(s, 0, _strlen_recursion(s)));
+	return (check_pal(s, 0, is_palindrome(s)));
 }
 
-/**
- * _strlen_recursion - returns the length of a string
- * @s: string to calculate the length of
- *
- * Return: length of the string
- */
-int _strlen_recursion(char *s)
+int is_palindrome(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	return (1 + _strlen_recursion(s + 1));
+	return (1 + is palindrome(s + 1));
 }
 
 /**
