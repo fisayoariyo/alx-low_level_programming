a53349b33402428be36c90700a3a2bb640cae910
Adefisayomi <adefisayomiariyo@gmail.com>
2023-03-14 14:24:45 -0700

    6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,64 +1,154 @@
+include <stdlib.h>
+		
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+		
 
+		
 /**
- * strtow - concatenates all the arguments of your program
- *@str: string
- *@av: arguments
- * Return: a pointer to a new string
- */
-char **strtow(char *str)
+		
+ *  * count_word - helper function to count the number of words in a string
+		
+ *   * @s: string to evaluate
+		
+ *    *
+		
+ *     * Return: number of words
+		
+ *      */
+		
+int count_word(char *s)
+		
 {
-	int i, w, j, k, count, m, wordf;
-	char **p;
-	char *x;
+		
+		int flag, c, w;
+		
 
-	w = 0;
-	j = 0;
-	i = 0;
-	count = 0;
-	if (*str == '\0' || str == NULL)
-		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
-			w++;
-	}
-	p = (char **)malloc((w + 1) * sizeof(char *));
-	if (p == NULL)
-		return (NULL);
-	for (wordf = 0; str[wordf] && j <= w; wordf++)
-	{
-		count = 0;
-		if (str[wordf] != ' ')
-		{
-			for (i = wordf ; str[i] != '\0'; i++)
-			{
-				if (str[i] == ' ')
-					break;
-				count++;
-			}
-			*(p + j) = (char *)malloc((count + 1) * sizeof(char));
-			if (*(p + j) == NULL)
-			{
-				for (k = 0; k <= j; k++)
-				{
-					x = p[k];
-					free(x);
-				}
-				free(p);
-				return (NULL);
-			}
-			for (m = 0; wordf < i; wordf++)
-			{
-				p[j][m] = str[wordf];
-				m++;
-			}
-			p[j][m] = '\0';
-			j++;
-		}
-	}
-	p[j] = NULL;
-	return (p);
+		
+			flag = 0;
+		
+				w = 0;
+		
+
+		
+					for (c = 0; s[c] != '\0'; c++)
+		
+							{
+		
+										if (s[c] == ' ')
+		
+														flag = 0;
+		
+												else if (flag == 0)
+		
+															{
+		
+																			flag = 1;
+		
+																						w++;
+		
+																								}
+		
+													}
+		
+
+		
+						return (w);
+		
 }
+		
+/**
+		
+ * **strtow - splits a string into words
+		
+ * @str: string to split
+		
+ *
+		
+ * Return: pointer to an array of strings (Success)
+		
+ * or NULL (Error)
+		
+ */
+		
+char **strtow(char *str)
+		
+{
+		
+	char **matrix, *tmp;
+		
+	int i, k = 0, len = 0, words, c = 0, start, end;
+		
+
+		
+	while (*(str + len))
+		
+		len++;
+		
+	words = count_word(str);
+		
+	if (words == 0)
+		
+		return (NULL);
+		
+
+		
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+		
+	if (matrix == NULL)
+		
+		return (NULL);
+		
+
+		
+	for (i = 0; i <= len; i++)
+		
+	{
+		
+		if (str[i] == ' ' || str[i] == '\0')
+		
+		{
+		
+			if (c)
+		
+			{
+		
+				end = i;
+		
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+		
+				if (tmp == NULL)
+		
+					return (NULL);
+		
+				while (start < end)
+		
+					*tmp++ = str[start++];
+		
+				*tmp = '\0';
+		
+				matrix[k] = tmp - c;
+		
+				k++;
+		
+				c = 0;
+		
+			}
+		
+		}
+		
+		else if (c++ == 0)
+		
+			start = i;
+		
+	}
+		
+
+		
+	matrix[k] = NULL;
+		
+
+		
+	return (matrix);
+		
+}
+
