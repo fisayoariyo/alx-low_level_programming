e44b8db7d8d84f6101cd093605fa2abcdcea55d3
Adefisayomi <adefisayomiariyo@gmail.com>
2023-03-16 14:19:12 -0700

    all

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,33 +1,38 @@
-#include <stdlib.h>
+#include "main.h"
 #include <stdio.h>
-/**
-  * _abs - get absolute value
-  * @n: number to calculate
-  * Return: absolute value of the number
-  */
-int _abs(int n)
-{
-	n < 0 ? (n *= -1) : (n = n);
-	return (n);
-}
-/**
-  * array_range - creates an array of integers
-  * @min: minimum value
-  * @max: maximum value
-  * Return: pointer to array of integers
-  */
-int *array_range(int min, int max)
-{
-	int *p;
-	int range, i;
+#include <stdlib.h>
+#include <limits.h>
 
-	if (min > max)
-		return (NULL);
-	range = max - min + 1;
-	p = malloc(range * sizeof(*p));
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; i < range; min++, i++)
-		p[i] = min;
-	return (p);
+/**
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
+**/
+
+int main(int argc, char *argv[])
+{
+	unsigned long mul;
+	int i, j;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	mul = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", mul);
+	return (0);
 }
