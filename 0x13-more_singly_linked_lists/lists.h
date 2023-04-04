fb9ac754fb7ab88a54189b7c37bee1aede2d769b
Adefisayomi <ariyoadefisayomi@gmail.com>
2023-04-04 05:43:36 -0700

    lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,8 +15,9 @@
 typedef struct listint_s
 {
 	int n;
-	struct listint_s*next;
-}listint_t;
+	struct listint_s *next;
+}
+	listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -32,6 +33,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
-listint_t *find_listint_loop(listint_t *head)
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
