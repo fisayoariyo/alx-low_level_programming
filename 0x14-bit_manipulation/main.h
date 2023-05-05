4bb0c945c4a5135be5c566ec6d363cb9adc3c5ad
Adefisayomi <ariyoadefisayomi@gmail.com>
2023-05-05 15:33:14 -0700

    compile

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,8 +3,7 @@
 
 
 unsigned int binary_to_uint(const char *b);
-int _putchar(char c); 
-int _strlen(const char *s)
+int _putchar(char c);
 int putchar(int c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
