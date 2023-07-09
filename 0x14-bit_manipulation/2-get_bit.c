fa48a294dad3553a24e6fa156d883d12382e2300
Adefisayomi <ariyoadefisayomi@gmail.com>
2023-07-09 05:08:12 -0700

    2_get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,13 +9,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit_val;
-
-	if (index > 63)
+	if (index > (sizeof(n) * 8))
 		return (-1);
 
-	bit_val = (n >> index) & 1;
-
-	return (bit_val);
+	n  = n >> index;
+	if (n & 1)
+		return (1);
+	else
+		return (0);
 }
-
