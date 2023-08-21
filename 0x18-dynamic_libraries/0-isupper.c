51031b8acdf42602e62d828807f69cbbefa6c016
Adefisayomi <ariyoadefisayomi@gmail.com>
2023-08-21 13:23:28 -0700

    0x18d

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * _isupper - Check if a letter is upper
+  * @c: The number to be checked
+  *
+  * Return: 1 for upper letter or 0 for any else
+  */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
